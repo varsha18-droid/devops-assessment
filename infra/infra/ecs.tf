0e1257acc651be7c973b3f7c1c54c8c2d4b093bd
varsha18-droid <varsha.sharma180499@gmail.com>
2026-07-06 09:40:42 +0530

    Update ecs.tf

--- infra/infra/ecs.tf
+++ infra/infra/ecs.tf
@@ -28,7 +28,7 @@ resource "aws_ecs_task_definition" "task" {
   cpu    = "256"
   memory = "512"
 
-  execution_role_arn = "arn:aws:iam::<ACCOUNT_ID>:role/ecsTaskExecutionRole"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
