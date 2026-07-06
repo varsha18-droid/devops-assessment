e32089381f0035c0d795fc274942d662a90b952f
varsha18-droid <varsha.sharma180499@gmail.com>
2026-07-06 09:25:07 +0530

    Create variables.tf

---
+++ infra/infra/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  type    = string
+  default = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  type    = string
+  default = "10.0.4.0/24"
+}
+
+variable "db_username" {
+  type    = string
+  default = "postgres"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
